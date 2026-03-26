b7c463b963a1c4bbe4d387034c62425ed1737177
Dohyeon Seong <dhcastle1@g.yju.ac.kr>
2026-03-26 15:39:27 +0900

    ch03-practice\p07.py 엽습

--- practice_test/ch03-practice/p07.py
+++ practice_test/ch03-practice/p07.py
@@ -1,14 +1,12 @@
 """
 실습 7: 할인 가격 계산
 """
-# TODO: 다시 하셈 ㅇㅇ
 
+# 총액
 total = 0
-
 # 상품 1: 노트북 1200000원, 10% 할인
 price1 = 1200000
-discount1= 0.1
-
+discount1 = 0.1
 # 상품 2: 마우스 35000원, 20% 할인
 price2 = 35000
 discount2 = 0.2
@@ -16,19 +14,28 @@ discount2 = 0.2
 # 상품 3: 키보드 55000원, 15% 할인
 price3 = 55000
 discount3 = 0.15
-# 할인 가격1 = 1200000 * (1 - 0.1)
-price_discount1 = price1 * (1 - discount1)
-# 할인 가격2 = 35000 * (1 - 0.2)
-price_discount2 = price2 * (1 - discount2)
-# 할인 가격3 = 55000 * (1 - 0.15)
-price_discount3 = price3 * (1 - discount3)
-# 총액 계산
-print(f"노트북: {int(price_discount1)}원")
-print(f"마우스: {int(price_discount2)}원")
-print(f"키보드: {int(price_discount3)}원")
 
-price_discount1 += price_discount2
-price_discount1 += price_discount3
-print(f"총 결제 금액: {int(price_discount1)}원")
+# 상품 1 = 1200000 * (1 - 0.1)
+sale1 = int(price1 * (1 - discount1))
+# 상품 2 = 35000 * (1 - 0.2)
+sale2 = int(price2 * (1 - discount2))
+# 상품 3 = 55000 * (1 - 0.15)
+sale3 = int(price3 * (1 - discount3))
 
 
+# 총액 += 상품 1
+total += sale1
+# 총액 += 상품 2
+total += sale2
+# 총액 += 상품 3
+total += sale3
+
+
+# 노트북: 1080000원
+print("노트북: " + str(sale1) + "원")
+# 마우스: 28000원
+print("마우스: " + str(sale2) + "원") 
+# 키보드: 46750원
+print("키보드: " + str(sale3) + "원")
+# 총 결제 금액: ㅇㅇ
+print("총 결제 금액: " + str(total) + "원")
