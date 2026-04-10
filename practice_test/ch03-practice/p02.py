4c3a1aae8bfff1f646d51cd520e01841db35c3fd
Dohyeon Seong <dhcastle1@g.yju.ac.kr>
2026-04-10 19:56:51 +0900

    ch04-p01~p04.py 작성

--- practice_test/ch03-practice/p02.py
+++ practice_test/ch03-practice/p02.py
@@ -14,15 +14,15 @@ print("거스름돈: " + str(change) + "원")
 # 1000원 개수 = 거소름돈 // 1000
 bill_1000 = change // 1000
 # 거스름돈 = 거스름돈 % 1000
-change1 = change % 1000
+change = change % 1000
 
 # 500원 개수 = 거스름돈 // 500
-coin_500 = change1 // 500
+coin_500 = change // 500
 # 거스름돈 = 거스름돈 % 500
-change1 = change1 % 500
+change = change % 500
 
 # 100원 개수 = 거스름돈 // 100
-coin_100 = change1 // 100
+coin_100 = change // 100
 
 # 1000원 개수 출력
 print("1000원: " + str(bill_1000) + "개")
---
+++ practice_test/ch04-practice/ch04_test.py
@@ -0,0 +1,16 @@
+# for + 리스트 - 요소를 하나씩 꺼내며 반복
+
+fruits = ["사과", "바나나", "포도"]
+
+for fruit in fruits:
+    print(fruit)
+
+print("---")
+
+numbers = [10, 20, 30, 40, 50]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("합계:", total)
